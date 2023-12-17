a0072faef2d66df4bfeab0180f77182756589acd
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-12-17 19:53:53 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Categories from "./components/Category"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+    <Categories />
+    <Footer/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Hero from "./Hero"
+import Nav from "./Nav"
+
+
+
+const Header = () => {
+  return (
+   <>
+    <Nav/>
+   <Hero/>
+  
+   
+   </>
+  )
+}
+
+export default Header
